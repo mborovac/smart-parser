56427fff9bc6e7b0699e6968876cbe545b47d3ee
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-07 11:50:21 +0100

    throw exception in LogParser initializer if no log file was given

--- lib/smart_log_parser/log_parser.rb
+++ lib/smart_log_parser/log_parser.rb
@@ -1,6 +1,10 @@
 module SmartLogParser
   class LogParser
     def initialize(log_file)
+      if log_file.nil? || log_file.empty?
+        raise SmartLogParser::SmartLogParserException.new "No log file given!"
+      end
+      @log_file = log_file
     end
   end
 end
