a1d59b1ff18834811f8195a49cb5874152181dfc
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-07 20:04:48 +0100

    add SmartLogOrderer initialize param check and raise not_implemented_error on its interface methods

--- lib/smart_log_parser/orderers/smart_log_orderer.rb
+++ lib/smart_log_parser/orderers/smart_log_orderer.rb
@@ -1,9 +1,17 @@
+require_relative '../exceptions/smart_log_parser_exception'
+require_relative '../helpers/interface_helper'
+
 module SmartLogParser
   class SmartLogOrderer
+    include SmartLogParser::InterfaceHelper
+
     def initialize(data)
+      raise SmartLogParser::SmartLogParserException.new 'Data must exist!' if (data.nil? || data.empty?)
+      @data = data
     end
 
     def order
+      not_implemented_error(self.class.name, __callee__)
     end
   end
 end
