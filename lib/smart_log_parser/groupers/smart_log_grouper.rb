a1d220bfd405e593118887553e2f6180236cb3e8
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-07 17:11:49 +0100

    raise not_implemented_error on SmartLogGrouper interface methods

--- lib/smart_log_parser/groupers/smart_log_grouper.rb
+++ lib/smart_log_parser/groupers/smart_log_grouper.rb
@@ -3,12 +3,14 @@ require_relative '../helpers/interface_helper'
 
 module SmartLogParser
   class SmartLogGrouper
+    include SmartLogParser::InterfaceHelper
 
     def initialize(reader)
       @reader = reader
     end
 
     def group
+      not_implemented_error(self.class.name, __callee__)
     end
   end
 end
