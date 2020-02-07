26f2124ecc874ceba83163df306e54a52fb24bd5
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-07 16:00:18 +0100

    add InterfaceHelper#not_implemented_error implementation

--- lib/smart_log_parser/helpers/interface_helper.rb
+++ lib/smart_log_parser/helpers/interface_helper.rb
@@ -1,8 +1,11 @@
+require_relative '../exceptions/smart_log_parser_exception'
+
 module SmartLogParser
   module InterfaceHelper
     protected
 
       def not_implemented_error(class_name, method = '')
+        raise SmartLogParser::SmartLogParserException.new "#{class_name} method #{method} not implemented"
       end
   end
 end
