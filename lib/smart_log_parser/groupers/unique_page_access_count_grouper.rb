9ab48d8bfff9ba4c5df215f54e27145151bc49e1
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-07 19:48:04 +0100

    add UniquePageAccessCountGrouper#group implementation

--- lib/smart_log_parser/groupers/unique_page_access_count_grouper.rb
+++ lib/smart_log_parser/groupers/unique_page_access_count_grouper.rb
@@ -1,6 +1,19 @@
+require_relative 'smart_log_grouper'
+
 module SmartLogParser
-  class UniquePageAccessCountGrouper
+  class UniquePageAccessCountGrouper < SmartLogParser::SmartLogGrouper
     def group
+      user_page_access_count = Hash.new(0)
+      unique_page_access_count = Hash.new(0)
+
+      @reader.read_lines do |page, user|
+        user_page_key = "#{user}_#{page}"
+
+        unique_page_access_count[page] += 1 if user_page_access_count[user_page_key] == 0
+        user_page_access_count[user_page_key] += 1
+      end
+
+      unique_page_access_count
     end
   end
 end
