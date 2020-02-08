0e91c239c1f760005ff8bad809981f02403381d6
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-08 11:18:14 +0100

    add LogParserPrinter#print_visits implementation

--- lib/smart_log_parser/io/log_parser_printer.rb
+++ lib/smart_log_parser/io/log_parser_printer.rb
@@ -1,6 +1,10 @@
 module SmartLogParser
   class LogParserPrinter
     def self.print_visits(data_array, description, follower_text, order)
+      print_array = []
+      print_array << "#{description} ordered in #{order}"
+      data_array.each{|page, count| print_array << "#{page} #{count} #{follower_text}" }
+      puts print_array.join("\n")
     end
   end
 end
