15ba2b12a31b17a3921cd74606c1d6f8ec39040f
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-08 23:22:34 +0100

    add unique_user_visits_count_grouper implementation

--- lib/smart_log_parser/groupers/unique_user_visits_count_grouper.rb
+++ lib/smart_log_parser/groupers/unique_user_visits_count_grouper.rb
@@ -1,12 +1,27 @@
+require_relative 'smart_log_grouper'
+
 module SmartLogParser
   class UniqueUserVisitsCountGrouper < SmartLogGrouper
     def group
+      user_page_access_count = Hash.new(0)
+      unique_user_page_visit_count = Hash.new(0)
+
+      @reader.read_lines do |page, user|
+        user_page_key = "#{user}_#{page}"
+
+        unique_user_page_visit_count[user] += 1 if user_page_access_count[user_page_key] == 0
+        user_page_access_count[user_page_key] += 1
+      end
+
+      unique_user_page_visit_count
     end
 
     def self.description
+      'List of unique page visits per user'
     end
 
     def self.follower_text
+      'unique page visits'
     end
   end
 end
