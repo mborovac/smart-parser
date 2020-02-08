633c030eb7b38933d3bec67f063098470a9c1cf0
Marko Borovac <marko.borovac.zg@gmail.com>
2020-02-08 15:52:53 +0100

    add LogParser#parse implementation

--- lib/smart_log_parser/log_parser.rb
+++ lib/smart_log_parser/log_parser.rb
@@ -8,6 +8,10 @@ module SmartLogParser
     end
 
     def parse(description, follower_text, ordering_description)
+      reader = SmartLogParser::LineByLineReader.new(@log_file)
+      grouped_data = SmartLogParser::PageAccessCountGrouper.new(reader).group
+      ordered_data = SmartLogParser::DescendingOrderer.new(grouped_data).order
+      SmartLogParser::LogParserPrinter.print_visits(ordered_data, description, follower_text, ordering_description)
     end
   end
 end
